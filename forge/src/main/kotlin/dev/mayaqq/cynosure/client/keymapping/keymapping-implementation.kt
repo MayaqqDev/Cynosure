41c5247cb97c33813902793730966d6ceb5212c1
Mayaqq <i@mayaqq.dev>
2025-09-24 12:52:24 +0200

    fixed keymapping registration on forge

--- forge/src/main/kotlin/dev/mayaqq/cynosure/client/keymapping/keymapping-implementation.kt
+++ forge/src/main/kotlin/dev/mayaqq/cynosure/client/keymapping/keymapping-implementation.kt
@@ -1,6 +1,10 @@
+@file:EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
 package dev.mayaqq.cynosure.client.keymapping
 
+import dev.mayaqq.cynosure.Cynosure
 import dev.mayaqq.cynosure.MODID
+import dev.mayaqq.cynosure.client.keymapping.KeyMappingRegistryImpl.mappings
+import dev.mayaqq.cynosure.client.keymapping.KeyMappingRegistryImpl.registered
 import net.minecraft.client.KeyMapping
 import net.minecraft.client.Minecraft
 import net.minecraftforge.api.distmarker.Dist
@@ -9,11 +13,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import org.apache.commons.lang3.ArrayUtils
 
-@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
 internal object KeyMappingRegistryImpl : KeyMappingRegistry {
 
-    private val mappings = mutableListOf<KeyMapping>()
-    private var registered = false
+    internal val mappings = mutableListOf<KeyMapping>()
+    internal var registered = false
 
     /**
      * Register a key mapping
@@ -23,16 +26,17 @@ internal object KeyMappingRegistryImpl : KeyMappingRegistry {
         if (registered) {
             val options = Minecraft.getInstance().options
             options.keyMappings = ArrayUtils.add(options.keyMappings, mapping)
+            Cynosure.info("Keymapping not registered properly: ${mapping.name}")
         } else {
             mappings.add(mapping)
         }
         return mapping
     }
+}
 
-    @SubscribeEvent
-    fun onKeyMappingRegister(event: RegisterKeyMappingsEvent) {
-        mappings.forEach(event::register)
-        mappings.clear()
-        registered = true
-    }
+@SubscribeEvent
+public fun onKeyMappingRegister(event: RegisterKeyMappingsEvent) {
+    mappings.forEach(event::register)
+    mappings.clear()
+    registered = true
 }
