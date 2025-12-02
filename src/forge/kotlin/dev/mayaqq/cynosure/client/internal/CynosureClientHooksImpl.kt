2ff9d896299ac6d7ec83eea440511ce600819f08
Mayaqq <i@mayaqq.dev>
2025-12-02 21:03:03 +0100

    Probably fixed event listeners as well

--- src/forge/kotlin/dev/mayaqq/cynosure/client/internal/CynosureClientHooksImpl.kt
+++ src/forge/kotlin/dev/mayaqq/cynosure/client/internal/CynosureClientHooksImpl.kt
@@ -2,6 +2,9 @@ package dev.mayaqq.cynosure.client.internal
 
 import dev.mayaqq.cynosure.CynosureInternal
 import dev.mayaqq.cynosure.MODID
+import dev.mayaqq.cynosure.client.internal.CynosureClientHooksEventSubscriber.CLIENT_RELOAD_LISTENERS
+import dev.mayaqq.cynosure.client.internal.CynosureClientHooksEventSubscriber.DEFERRED_CLIENT_RELOAD_LISTENERS
+import dev.mayaqq.cynosure.client.internal.CynosureClientHooksEventSubscriber.resourceLoaderEventFired
 import net.minecraft.client.Minecraft
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.packs.resources.PreparableReloadListener
@@ -12,14 +15,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import net.minecraftforge.fml.loading.FMLEnvironment
 
-@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
 @CynosureInternal
 internal class CynosureClientHooksImpl : CynosureClientHooks {
-
-    private val CLIENT_RELOAD_LISTENERS: MutableMap<ResourceLocation, PreparableReloadListener> = mutableMapOf()
-    private val DEFERRED_CLIENT_RELOAD_LISTENERS: MutableList<PreparableReloadListener> = mutableListOf()
-    private var resourceLoaderEventFired = false
-
     override fun registerReloadListener(id: ResourceLocation, listener: PreparableReloadListener) {
         require(!CLIENT_RELOAD_LISTENERS.containsKey(id)) { "Attempting to register duplicate listener id $id" }
         require(CLIENT_RELOAD_LISTENERS[id] != listener) { "Attempting to register the same listener twice" }
@@ -33,9 +30,16 @@ internal class CynosureClientHooksImpl : CynosureClientHooks {
             }
         }
     }
+}
+
+@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
+public object CynosureClientHooksEventSubscriber {
+    internal val CLIENT_RELOAD_LISTENERS: MutableMap<ResourceLocation, PreparableReloadListener> = mutableMapOf()
+    internal val DEFERRED_CLIENT_RELOAD_LISTENERS: MutableList<PreparableReloadListener> = mutableListOf()
+    internal var resourceLoaderEventFired = false
 
     @SubscribeEvent
-    fun onRegisterReloadListeners(event: RegisterClientReloadListenersEvent) {
+    public fun onRegisterReloadListeners(event: RegisterClientReloadListenersEvent) {
         DEFERRED_CLIENT_RELOAD_LISTENERS.forEach { event.registerReloadListener(it) }
         DEFERRED_CLIENT_RELOAD_LISTENERS.clear()
         resourceLoaderEventFired = true
