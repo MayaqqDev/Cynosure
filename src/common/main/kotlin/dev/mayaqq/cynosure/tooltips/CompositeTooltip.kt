1bc9addbb0b9dd4f9fe341abbd22f9e9c9d52aa3
Mayaqq <i@mayaqq.dev>
2026-07-10 12:57:49 +0200

    fix: Righttt generics

--- src/common/main/kotlin/dev/mayaqq/cynosure/tooltips/CompositeTooltip.kt
+++ src/common/main/kotlin/dev/mayaqq/cynosure/tooltips/CompositeTooltip.kt
@@ -6,10 +6,11 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 
-public class CompositeTooltip(public val tooltips: List<CustomTooltip>) : CustomTooltip {
+public class CompositeTooltip<T : CustomTooltip>(public val tooltips: List<T>) : CustomTooltip {
     override fun MutableList<Component>.modifyTooltip(stack: ItemStack, player: Player?, flags: TooltipFlag) {
         tooltips.forEach { with(it) { modifyTooltip(stack, player, flags) } }
     }
 
-    public constructor(vararg tooltips: CustomTooltip): this(tooltips.toList())
+    public constructor(vararg tooltips: T): this(tooltips.toList())
+
 }
