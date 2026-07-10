66905717e21d7807e9f2469b4ff9dcfad88f11e9
Mayaqq <i@mayaqq.dev>
2026-07-10 14:35:09 +0200

    feat: Tinyfont in cynosure yay

---
+++ src/common/main/kotlin/dev/mayaqq/cynosure/text/Fonts.kt
@@ -0,0 +1,8 @@
+package dev.mayaqq.cynosure.text
+
+import dev.mayaqq.cynosure.modId
+import net.minecraft.resources.ResourceLocation
+
+public object CynosureFonts {
+    public val TinyFont: ResourceLocation = modId("tinyfont")
+}
