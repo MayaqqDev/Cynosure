5b81bce64e770bb0a886e6c92aac248413f0b66c
Mayaqq <i@mayaqq.dev>
2026-08-01 21:21:52 +0200

    chore: clientOnlyNoDatagen on RenderlayerMap

--- src/common/forgeLike/main/kotlin/dev/mayaqq/cynosure/client/render/RenderLayerMapImpl.kt
+++ src/common/forgeLike/main/kotlin/dev/mayaqq/cynosure/client/render/RenderLayerMapImpl.kt
@@ -1,15 +1,20 @@
 package dev.mayaqq.cynosure.client.render
 
+import invoke.kitty.kritter.utils.clientOnlyNoDatagen
 import net.minecraft.client.renderer.ItemBlockRenderTypes
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.world.level.material.Fluid
 
 public object RenderLayerMapImpl : RenderLayerMap {
     override fun putFluid(fluid: Fluid, renderType: RenderType) {
-        ItemBlockRenderTypes.setRenderLayer(fluid, renderType)
+        clientOnlyNoDatagen {
+            ItemBlockRenderTypes.setRenderLayer(fluid, renderType)
+        }
     }
 
     override fun putFluids(renderType: RenderType, vararg fluids: Fluid) {
-        fluids.forEach { ItemBlockRenderTypes.setRenderLayer(it, renderType) }
+        clientOnlyNoDatagen {
+            fluids.forEach { ItemBlockRenderTypes.setRenderLayer(it, renderType) }
+        }
     }
 }
